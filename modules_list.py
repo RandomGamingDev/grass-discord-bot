748092a2cf648574a1157673868715ba6589253c
Knectoparapolosis <128651718+Knectoparapolosis@users.noreply.github.com>
2024-08-20 03:40:12 -0400

    update of economy module to handle !balance command, roulette bug fixes to fix outlier 0,00 split + better error handling

--- modules_list.py
+++ modules_list.py
@@ -1,10 +1,11 @@
 from typing import List
 
 import module
-from modules import counter, ping, randchoice, roulette
+from modules import counter, ping, randchoice, roulette, economy
 
 modules: List[module.Module] = [
     ping.PingModule(),
+    economy.EconomyModule(),
     roulette.RouletteModule(),
     counter.CounterModule(),
     randchoice.RandomModule(),
